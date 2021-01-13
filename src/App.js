78d2f461022f63f553ccce10db5776f252ac50da
Ryan Kavanaugh <ryankavanaugh@Ryans-MBP.lan>
2021-01-12 21:19:29 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+import { Layout } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Footer, Sider, Content } = Layout;
+
+class App extends Component {
+
+  postMsg() {
+    console.log('posting message');
+    // this.setState(state => ({
+    // }));
+  }
+
+  render() {
+    return (
+      <div className="App"> 
+      <Layout>
+      <Header>Seetha & Ryan's Feeds Project</Header>
+      <Layout>
+      <Sider style={{background:'white'}}>
+       <div id="sidebarContainer"> 
+       Toggle Between Feeds Here
+       </div>
+        </Sider>
+      <Layout>
+        <Content><h1>Our Feeds</h1></Content>
+        <div id="postContainer">
+        //Post Section//
+        <div></div>
+        <div id="textBox">
+          Enter Post  Here
+        </div>
+        <div></div>
+        <button onClick={this.postMsg}>
+        Post
+        </button>
+        
+        </div>
+        <Footer>        
+          <div id="container">
+          Personal Timeline & Global Feed
+        </div></Footer>
+      </Layout>
+    </Layout>
+    </Layout>
+      </div>      
+    );
+  }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Person = () => {
+    return <p>I'm an example person component and I am {Math.floor(Math.random() * 70)} years old!</p>
+};
+
+export default Person;
