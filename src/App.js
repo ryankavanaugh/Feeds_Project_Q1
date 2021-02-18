1895deac0c1a180be7a971e1baaa0e1f1070ee06
Ryan Kavanaugh <ryankavanaugh@Ryans-MBP.lan>
2021-02-18 17:27:22 -0600

    new update 011

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
 import React, { Component, useState } from 'react';
 import './App.css';
-import CustomActivity from './CustomActivity/CustomActivity';
+//import CustomActivity from './CustomActivity/CustomActivity';
 import { Layout } from 'antd';
 import 'react-activity-feed/dist/index.css';
-import { StreamApp, StatusUpdateForm, NotificationDropdown, FlatFeed, Activity } from 'react-activity-feed';
+import { StreamApp, StatusUpdateForm, NotificationDropdown, LikeButton,
+  FlatFeed, Activity, CommentItem, CommentField, CommentList } from 'react-activity-feed';
 import { connect } from 'getstream';
-
-
-// To do:
-// connect the feed
-// add activities to the feed
-// 
-
-
+const stream = require('getstream');
 const { Header, Footer, Sider, Content } = Layout;
 
-class App extends Component {
+// To do
+// login that takes you to your feed
+// toggle buttons that change feed
+// aggregated notification feed that takes you to the post
+// photos, comments, + reactions, will CDN be an issue with photos?
 
+const CustomActivity = (props) => {
+  return (
+    <Activity
+      {...props}
+      Footer={
+      
+        <div>
+        <LikeButton {...props} />
+        <CommentField
+                      activity={props.activity}
+                      // onAddReaction={props.onAddReaction} 
+                      />
+                      <CommentList
+                      activityId={props.activity.id}
+                      CommentItem={(props) => (
+                       <div>
+                          <CommentItem {...props} />
+                          <LikeButton
+                            reaction={props.comment}
+                            {...props}
+                        />
+                        </div>)} />
+        </div>
+
+        // <div>
+        // <LikeButton {...props} />
+        // <CommentField
+        // activity={props.activity}
+        // onAddReaction={props.onAddReaction} />
+        // </div>
+
+        
+      }
+    />
+  );
+};
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -28,18 +63,21 @@ class App extends Component {
       userID: 'ryan'
     };
   }
-
   getToken(userName){
-    fetch("http://localhost:1801/feedstoken?req=" + userName, {method: 'POST'})
-    .then(res => res.json())
-    .then(data => this.setState({token: data.payload}))
-    console.log("token:" + this.state.token)
+   fetch("http://localhost:3002/token",
+    {
+      method: "post",
+      headers: { "Content-Type" : "application/json"},
+      body: JSON.stringify({
+        input: 'seetha'
+      })
+    }).then(response => response.json())
+    .then(token => { return token })
   }
-
   // life cycle hook
   componentDidMount() {
-    this.getToken('ryan')
-    console.log(this.state.token)    
+    this.getToken(this.state.userID)
+    console.log(this.state.token)
   }
 
   updateInputValue(newInputValue) {
@@ -47,117 +85,143 @@ class App extends Component {
     console.log(this.state.inputValue);
   }
 
-  postMsg = async () => {
-    this.getToken('ryan');
-    console.log(this.state.inputValue);
+  createFollowRelationship = async () => {
+    //userOne follows UserTwo
     const key = 'qaq9tzbfa59s'
     const appID = '100501'
-    const client = connect(key, this.state.token, appID)
-    console.log(client)
-    const feed = client.feed('user', 'ryan', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidW5kZWZpbmVkIn0.Pw_RkXrzD7zXqsYixYYkQ7LqCefmvyyWRGm_3gsuSQ4')
-    console.log(feed)
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamFjayJ9.vG9zkuZvcQpRftbYFpNdxBKNxvCNV-gG3SJtJmP_bUg';
+    const userOne = 'jack';
+    const userTwo = 'ryan';
+    const client = stream.connect( key, token, appID);
+    // const feed = client.feed('timeline', this.state.userID, token);
+    const feed = client.feed('timeline', userOne, token);
+    feed.follow('timeline', userTwo);
+  }
+
+  postMsg = async () => {
+    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamFjayJ9.vG9zkuZvcQpRftbYFpNdxBKNxvCNV-gG3SJtJmP_bUg';
+    // // const userID='seetha';
+    // // const userID = this.state.userID
+
+    const key = 'qaq9tzbfa59s'
+    const appID = '100501'
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamFjayJ9.vG9zkuZvcQpRftbYFpNdxBKNxvCNV-gG3SJtJmP_bUg';
+    const userID = 'jack';
+    const client = stream.connect( key, token, appID);
+    // const feed = client.feed('timeline', this.state.userID, token);
+    const feed = client.feed('timeline', userID, token);
+    // await feed.follow('ryan');
+    
     const activityfeed = {
-      'actor': 'ryan',
+      'actor': "SU:"+userID,
       'verb': this.state.inputValue,
-      'object': 'Testing Feed',
+      'object': this.state.inputValue,
       'attachments': 'https://image.shutterstock.com/image-photo/portrait-tiger-black-background-600w-1543677188.jpg',
-      'time': '2020-11-21T20:30:45.123'
+      'time': new Date()
       };
-      console.log(activityfeed)
     const addactivity = await feed.addActivity(activityfeed);
-    console.log(addactivity);
-    // const add = feed.addActivity(addactivity)
-    // return feed
     }
 
-  render() {
+  postTimelineMsg = async () => {
+      const key = 'qaq9tzbfa59s'
+      const appID = '100501'
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicnlhbiJ9.Va9Thg7gl4GtW3z1Q18FsHxYL7P0rAbXJtzpQTAr4KQ';
+      const userID = 'ryan';
+      const client = stream.connect( key, token, appID);
+      // const feed = client.feed('timeline', this.state.userID, token);
+      const feed = client.feed('timeline', userID, token);
+      
+      
+      const activityfeed = {
+        'actor': "SU:"+userID,
+        'verb': this.state.inputValue,
+        'object': this.state.inputValue,
+        'attachments': 'https://image.shutterstock.com/image-photo/portrait-tiger-black-background-600w-1543677188.jpg',
+        'time': new Date()
+        };
+      const addactivity = await feed.addActivity(activityfeed);
+      }
 
+  render() {
     return (
-      <div className="App"> 
+      <div className="App">
       <Layout>
       <Header><h1>Seetha & Ryan's Feeds Project</h1></Header>
       <Layout>
       <Sider style={{background:'white'}}>
-       <div id="sidebarContainer"> 
-       Toggle Between Feeds Here
+       <div id="sidebarContainer">
+       Global Feed
+       Personal Timeline
+       Notification 
        </div>
         </Sider>
-      <Layout>
-        <Content><h1>Our Feeds</h1></Content>
+
+        <Content>
         <div id="postContainer">
         <br></br>
+        <b>Global Feed/Jack</b>
+        <br></br>
+        <br></br>
         <div></div>
-        {/* Here: this onChange function is listening for specific events, e means events */}
+        
         <textarea onChange={e => this.updateInputValue(e.target.value)}></textarea>
         <div></div>
         <br></br>
         <button onClick={() => this.postMsg()}>
         Post
         </button>
-        </div>
-        <Footer>        
-          <div id="container">
             <br></br>
-
-          Personal Timeline & Global Feed
           
-          {/* APP */}
-            {/* <StreamApp
-              apiKey="qaq9tzbfa59s"
-              appId="100501"
-             token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidW5kZWZpbmVkIn0.Pw_RkXrzD7zXqsYixYYkQ7LqCefmvyyWRGm_3gsuSQ4"
-            >
-              <NotificationDropdown notify/>
-              <FlatFeed
-                notify
-              />
-            </StreamApp>  */}
-        {/* APP */}
-
         <StreamApp
-            // apiKey="3ektw92qb4ew"
-            // appId="93749"
-            // token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2VldGhhIn0.ZDepaoDtrvJXaWAQhVxPOUzsIy7LG5T_qrEPZt9PPzE"
+            // const key = 'qaq9tzbfa59s'
+            // const appID = '100501'
+            // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamFjayJ9.vG9zkuZvcQpRftbYFpNdxBKNxvCNV-gG3SJtJmP_bUg';
+            //  apiKey="3ektw92qb4ew"
+            //  appId="93749"
+            //  token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2VldGhhIn0.ZDepaoDtrvJXaWAQhVxPOUzsIy7LG5T_qrEPZt9PPzE"
             apiKey="qaq9tzbfa59s"
             appId="100501"
-           token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidW5kZWZpbmVkIn0.Pw_RkXrzD7zXqsYixYYkQ7LqCefmvyyWRGm_3gsuSQ4"
-          
+           token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiamFjayJ9.vG9zkuZvcQpRftbYFpNdxBKNxvCNV-gG3SJtJmP_bUg"
         >
           <FlatFeed Activity={CustomActivity} notify />
           <StatusUpdateForm feedGroup="timeline" />
         </StreamApp>
-        
-        </div></Footer>
+        </div>
+
+        <div id="postContainer">
+          <br></br>
+        <b>Personal Timeline/Ryan</b> 
+        <br></br>
+        <br></br>
+        <div></div>
+        <textarea onChange={e => this.updateInputValue(e.target.value)}></textarea>
+        <div></div>
+        <br></br>
+        <button onClick={() => this.postTimelineMsg()}>
+        Post
+        </button>
+        <button onClick={() => this.createFollowRelationship()}>
+        Create Follow Relationship
+        </button>
+            <br></br>
+        <StreamApp
+            apiKey="qaq9tzbfa59s"
+            appId="100501"
+           token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicnlhbiJ9.Va9Thg7gl4GtW3z1Q18FsHxYL7P0rAbXJtzpQTAr4KQ"
+        >
+          <FlatFeed Activity={CustomActivity} notify />
+          <StatusUpdateForm feedGroup="timeline" />
+        </StreamApp>
+        </div>
+
+        </Content>
+        </Layout>
+        <Footer> Footer
+          </Footer>
       </Layout>
-    </Layout>
-    </Layout>
-      </div>      
+
+      </div>
     );
   }
 }
-
 export default App;
-
-
-
-
-
-// // next: interact with stream api (look at doing this inside life cycle hook or constructor)
-// // Feed.activity
-// // Check out life cycle hooks - always want to grab feed, so it can be stored as a value to manipulate, did component now??
-// // Server side end points - Seetha
-
-// const key = 'qaq9tzbfa59s'
-// const secret = '87hpnuzt45t9bac2t73u2thk2h8dzdt9w6tcyp7bzth9h5t7qnm6damrg97tfnnj'
-// const appID = '100501'
-// let stream = require('getstream');
-// let client = stream.connect(key, null, appID);
-// let userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicnlhbiJ9.Va9Thg7gl4GtW3z1Q18FsHxYL7P0rAbXJtzpQTAr4KQ'
-
-// const newFeed = client.feed('user', 'ryan', userToken);
-// // console.log(newFeed);
-// const activity = newFeed.addActivity({
-//   actor: 'ryan', 
-//   tweet: 'Hello world', 
-//   verb: 'tweet', 
-// })
